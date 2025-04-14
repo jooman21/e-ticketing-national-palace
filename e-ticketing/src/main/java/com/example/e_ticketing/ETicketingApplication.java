0ad689799f102c430842f992a3ea2651861257f7
jooman21 <jooman2121@gmail.com>
2025-04-14 11:15:47 +0300

    initalizing e-ticketing

---
+++ e-ticketing/src/main/java/com/example/e_ticketing/ETicketingApplication.java
@@ -0,0 +1,13 @@
+package com.example.e_ticketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ETicketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ETicketingApplication.class, args);
+	}
+
+}
